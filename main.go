a602c94e6019701f0388af8cf37482b0da4f2e92
cegal <camilo.garcia.larotta@gmail.com>
2019-03-26 16:31:35 -0400

    WIP: add fogleman/gg gif

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 	"image"
 	"image/color"
 	"image/color/palette"
@@ -11,6 +13,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/fogleman/gg"
 	"github.com/llgcode/draw2d/draw2dimg"
 	//NOTE: gfx is not using modules, so you'll have to work inside your $GOPATH
 	"github.com/peterhellberg/gfx"
@@ -19,26 +22,33 @@ import (
 func main() {
 	// DONE
 	// - create GIFs via image stdlib
-	// - create GIFs via gfx & draw2d
+	// - create GIFs via gfx & draw2d & gg
 	// TODO
-	// - write text in a GIF
+	// - write text in all GIFs
 	//   see:
 	//   https://stackoverflow.com/questions/38299930/how-to-add-a-simple-text-label-to-an-image-in-go
 	//   https://godoc.org/github.com/golang/freetype
 	//
-	// - benchmark which approach is fastest to create gifhub GIFs
+	// - benchmark which lib (gfx, draw2d, gg) is fastest to create gifhub GIFs
 
+	const (
+		w = 1000
+		h = 1000
+	)
 	// generate gif via peterhellberg/gfx
-	stdlibGIF("stdlib.gif")
+	stdlibGIF(w, h, "stdlib.gif")
 
-	// generate gif via peterhellberg/gfx
-	gfxGIF("gfx.gif")
+	// // generate gif via peterhellberg/gfx
+	gfxGIF(w, h, "gfx.gif")
 
-	// generate gif via llgcode/draw2d
-	draw2dGIF("draw2d.gif")
+	// // generate gif via llgcode/draw2d
+	draw2dGIF(w, h, "draw2d.gif")
+
+	// generate gif via
+	ggGIF(w, h, "gg.gif")
 }
 
-func gfxGIF(output string) {
+func gfxGIF(w, h int, output string) {
 	rand.Seed(time.Now().UnixNano())
 
 	var en4 = gfx.PaletteEN4
@@ -49,19 +59,19 @@ func gfxGIF(output string) {
 	// then add them to the gfx animation
 	for i := 0; i < 10; i++ {
 		p := gfx.Polygon{
-			{rand.Float64() * 1000, rand.Float64() * 1000},
-			{rand.Float64() * 1000, rand.Float64() * 1000},
-			{rand.Float64() * 1000, rand.Float64() * 1000},
-			{rand.Float64() * 1000, rand.Float64() * 1000},
+			{rand.Float64() * float64(w), rand.Float64() * float64(h)},
+			{rand.Float64() * float64(w), rand.Float64() * float64(h)},
+			{rand.Float64() * float64(w), rand.Float64() * float64(h)},
+			{rand.Float64() * float64(w), rand.Float64() * float64(h)},
 		}
 
-		m := gfx.NewPaletted(1000, 1000, en4, en4.Color(7))
+		m := gfx.NewPaletted(w, h, en4, en4.Color(7))
 		gfx.DrawPolygon(m, p, 0, en4.Color(2))
 
-		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*1000, rand.Float64()*1000), 25, en4.Color(0))
-		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*1000, rand.Float64()*1000), 25, en4.Color(0))
-		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*1000, rand.Float64()*1000), 25, en4.Color(1))
-		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*1000, rand.Float64()*1000), 25, en4.Color(1))
+		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*float64(w), rand.Float64()*float64(h)), 25, en4.Color(0))
+		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*float64(w), rand.Float64()*float64(h)), 25, en4.Color(0))
+		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*float64(w), rand.Float64()*float64(h)), 25, en4.Color(1))
+		gfx.DrawCircleFilled(m, gfx.V(rand.Float64()*float64(w), rand.Float64()*float64(h)), 25, en4.Color(1))
 
 		a.AddPalettedImage(m)
 	}
@@ -69,15 +79,15 @@ func gfxGIF(output string) {
 	a.SaveGIF(output)
 }
 
-func draw2dGIF(output string) {
+func draw2dGIF(w, h int, output string) {
 	lightGreen := color.RGBA{123, 201, 111, 0xff}
 	darkGreen := color.RGBA{108, 178, 103, 0xff}
 
 	// draw the images using an RGBA image and the graphic context of draw2d library
-	dest1 := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+	dest1 := image.NewRGBA(image.Rect(0, 0, w, h))
 	gcImg1 := draw2dimg.NewGraphicContext(dest1)
 
-	dest2 := image.NewRGBA(image.Rect(0, 0, 1000, 1000))
+	dest2 := image.NewRGBA(image.Rect(0, 0, w, h))
 	gcImg2 := draw2dimg.NewGraphicContext(dest2)
 
 	// draw polygon
@@ -109,15 +119,50 @@ func draw2dGIF(output string) {
 	gcImg2.LineTo(500, 900)
 	gcImg2.FillStroke()
 
-	// create a paletted image from the RGBA images
-	img1 := image.NewPaletted(dest1.Bounds(), palette.Plan9)
-	draw.Draw(img1, img1.Rect, dest1, dest1.Bounds().Min, draw.Src)
+	GIF(output, 25, dest1, dest2)
+}
 
-	img2 := image.NewPaletted(dest2.Bounds(), palette.Plan9)
-	draw.Draw(img2, img2.Rect, dest2, dest2.Bounds().Min, draw.Src)
+func ggGIF(w, h int, output string) {
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// encode the GIF
-	anim := gif.GIF{Delay: []int{25, 25}, Image: []*image.Paletted{img1, img2}}
+	face := truetype.NewFace(font, &truetype.Options{Size: 48})
+
+	dc1 := gg.NewContext(w, h)
+	dc1.SetFontFace(face)
+	dc1.SetRGB(1, 1, 1)
+	dc1.Clear()
+	dc1.SetRGB(0, 0, 0)
+	dc1.DrawStringAnchored("hello", 512, 512, 0.5, 0.5)
+
+	dc2 := gg.NewContext(w, h)
+	dc2.SetFontFace(face)
+	dc2.SetRGB(1, 1, 1)
+	dc2.Clear()
+	dc2.SetRGB(0, 0, 0)
+	dc2.DrawStringAnchored("world", 512, 512, 0.5, 0.5)
+
+	GIF(output, 35, dc1.Image(), dc2.Image())
+}
+
+// GIF creates an output gif file with the input images as frames
+func GIF(output string, delay int, images ...image.Image) {
+	numFrames := len(images)
+	palettedImgs := []*image.Paletted{}
+	for _, img := range images {
+		paletted := image.NewPaletted(img.Bounds(), palette.Plan9)
+		draw.Draw(paletted, paletted.Rect, img, img.Bounds().Min, draw.Src)
+		palettedImgs = append(palettedImgs, paletted)
+	}
+
+	var delays = make([]int, numFrames)
+	for i := 0; i < numFrames; i++ {
+		delays[i] = delay
+	}
+
+	anim := gif.GIF{Delay: delays, Image: palettedImgs}
 
 	f, err := os.Create(output)
 	if err != nil {
@@ -126,15 +171,13 @@ func draw2dGIF(output string) {
 	gif.EncodeAll(f, &anim)
 	f.Close()
 }
-
-func stdlibGIF(output string) {
-	h, w := 1000, 1000
+func stdlibGIF(w, h int, output string) {
 	numFrames := 5
 
 	var delays = make([]int, numFrames)
 	var frames = make([]*image.Paletted, numFrames)
 	for i := 0; i < numFrames; i++ {
-		delays[i] = 100
+		delays[i] = 50
 		frames[i] = randomFrame(h, w)
 	}
 
